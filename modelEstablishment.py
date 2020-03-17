1fbc273d28514153a7ccd8173749c6487d69ad7d
henhimicons <kanberra354562007@gmail.com>
2020-03-18 07:54:25 +0800

    model training

---
+++ modelEstablishment.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+
+class MyModel(tf.keras.Model):
+    def __init__(self):
+        super().__init__();
+        self.dense=tf.keras.layers.Dense(
+            units=1,
+            activation = None,
+            kernel_initializer = tf.zeros_initializer(),
+            bias_initializer=tf.zeros_initializer()
+        )
+    def call(self,input):
+        output = self.dense(input);
+        return output;
+
+model = MyModel()
+X = tf.constant([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
+y = tf.constant([[10.0], [20.0]])
+optimizer = tf.keras.optimizers.SGD(learning_rate=0.01)
+for i in range(100):
+    with tf.GradientTape() as tape:
+        y_pred = model(X)
+        loss = tf.reduce_mean(tf.square(y_pred-y))
+    grads = tape.gradient(loss,model.variables)
+    optimizer.apply_gradients(grads_and_vars=zip(grads,model.variables))
+print(model.variables)
---
+++ test.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+s = np.zeros((200,100))
+print(s.shape[0])
